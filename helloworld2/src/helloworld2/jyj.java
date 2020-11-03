ae14d18a166b8318c41168ac8c6dc56ee3d2ee2b
wku <wku@192.168.172.108>
2020-11-03 10:02:00 +0900

    yjcho branch change a code

--- helloworld2/src/helloworld2/jyj.java
+++ helloworld2/src/helloworld2/jyj.java
@@ -4,5 +4,6 @@ public class jyj {
 public static void main(String[] args) {
 	System.out.println("Hello World");
 	System.out.println("Cho Youngjin");
+	System.out.println("Computer SW engineering");
 }
 }
